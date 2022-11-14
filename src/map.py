b634a7701796dce3e9cb1cf559c696e264b7cbb2
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-11-14 00:05:53 -0500

    add map elements

--- src/map.py
+++ src/map.py
@@ -12,3 +12,34 @@ class Map:
         # Graph of the provinces, sea and neutral neighbours
         self.province_neighbours= Graph()
 
+
+    def add_nation(self, name: str, extension: float, contain: list, traits: list):
+        self.__detect_element(name, self.nationdict)
+        nat= Nation(name, extension, contain, traits)
+        self.nationdict[name]= nat
+        return nat
+
+    def add_province(self, name: str, extension: float, developmen: int, population: int, neighbours: list):
+        self.__detect_element(name, self.provincedict)
+        prov= Province(name, extension, developmen, population)
+        self.provincedict[name]= prov
+        self.province_neighbours.add_node(name)
+        self.__add_edges(name, neighbours)
+        return prov
+
+    def add_sea(self, name: str, extension: float, neighbours: list):
+        self.__detect_element(name, self.neutraldict)
+        sea= Sea(name, extension, neighbours)
+        self.seadict[name]= sea
+        self.province_neighbours.add_node(name)
+        self.__add_edges(name, neighbours)
+        return sea
+
+    def add_neutral(self, name: str, extension: float, neighbours: list):
+        self.__detect_element(name, self.provincedict)
+        neutral= Neutral(name, extension, neighbours)
+        self.provincedict[name]= neutral
+        self.province_neighbours.add_node(name)
+        self.__add_edges(name, neighbours)
+        return neutral
+
