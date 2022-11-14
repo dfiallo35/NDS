abf506e0e92ceac77b93c133b281c4767f6fa2e4
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-11-14 00:06:27 -0500

    map methods

--- src/map.py
+++ src/map.py
@@ -43,3 +43,45 @@ class Map:
         self.__add_edges(name, neighbours)
         return neutral
 
+
+    def nation_neighbours(self, nation: str):
+        """
+        Get the nation neighbours
+        :param nation: the nation name
+        :return: the nation neighbours
+        """
+        neighbours = []
+        for province in self.nationdict[nation].contain:
+            neighbours.extend(self.province_neighbours[province])
+        neighbours = list(set(neighbours))
+
+        nation_neighbours = []
+        for nat in self.nationdict:
+            for province in self.nationdict[nat].contain:
+                if province in neighbours:
+                    nation_neighbours.append(nat)
+        nation_neighbours = list(set(nation_neighbours))
+        return nation_neighbours
+
+    def __add_edges(self, province: str, neighbours: list):
+        """
+        Add edges to the element
+        :param element: the element
+        :param neighbour: the neighbour list
+        """
+        for neighbour in neighbours:
+            if neighbour in self.provincedict or neighbour in self.neutraldict or neighbour in self.seadict:
+                self.province_neighbours.add_edge(province, neighbour)
+            else:
+                raise Exception("Map element not found")
+
+
+
+    def __detect_element(self, name: str, elements_dict: dict):
+        """
+        Detect if an element is already in the map
+        :param name: the element name
+        :param elements_dict: the dictionary of the elements
+        """
+        if elements_dict.get(name) != None:
+            raise Exception("Element already exists")
