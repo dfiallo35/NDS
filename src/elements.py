47fd9752908182f8de14364d32c80d5e95bceca9
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-11-14 00:04:17 -0500

    Update elements.py

--- src/elements.py
+++ src/elements.py
@@ -10,37 +10,34 @@ class MapElement(Element):
     """
     Base class for all the map elements.
     """
-    def __init__(self, name, extension:float, forain):
+    def __init__(self, name):
         super().__init__(name)
-        self.forain = forain
-        self.extension = extension
 
 
 
 class Nation(MapElement):
-    def __init__(self, name: str, extension:float, contain: list, traits: list, forain: list):
-        super().__init__(name, extension, forain)
+    def __init__(self, name: str, contain: list, traits: list):
+        super().__init__(name)
         self.contain = contain
         self.traits = traits
-        self.forain = forain
         
 
 
 class Province(MapElement):
-    def __init__(self, name: str, extension:float, developmen: int, population: int, forain: list):
-        super().__init__(name, extension, forain)
+    def __init__(self, name: str, extension:float, developmen: int, population: int):
+        super().__init__(name, extension)
         self.developmen = developmen
         self.population = population
 
 
 class Sea(MapElement):
-    def __init__(self, name: str, extension:float, forain: list):
-        super().__init__(name, extension, forain)
+    def __init__(self, name: str, extension:float):
+        super().__init__(name, extension)
 
 
 class Neutral(MapElement):
-    def __init__(self, name: str, extension:float, forain: list):
-        super().__init__(name, extension, forain)
+    def __init__(self, name: str, extension:float):
+        super().__init__(name, extension)
 
 
 class Trait(Element):
