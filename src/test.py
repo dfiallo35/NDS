7a20e1a1fbab15dd8d5c1df8311f36ce9e1f1e36
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-23 00:26:04 -0500

    simulation test

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
-from map import *
-
+from elements.elements import *
+from elements.map import *
+from simulation import *
+from events import *
 
 m = Map()
 m.add_province('Matanzas', 10, 10, 120)
@@ -7,12 +9,33 @@ m.add_province('Habana', 14, 15, 240, ['Matanzas'])
 m.add_province('Camaguey', 12, 12, 100, ['Matanzas', 'Habana'])
 m.add_nation('Cuba', ['Matanzas', 'Habana', 'Camaguey'])
 m.add_province('Florida', 10, 10, 120)
-
+m.add_nation('USA', ['Florida'])
+m.add_province('Madrid', 10, 10, 120)
 
 m.update('Habana', development= 20)
-m.update('Cuba', provinces= ['Florida'])
 
-nat: Nation= m.nationdict['Cuba']
-print(m.mapelementsdict)
+class population_growth(Event):
+    def __init__(self):
+        self.distribution= Exponential(1)
+        self.enabled= True
+    
+    def execute(self, map: Map, **kwargs):
+        for province in map.provincedict.values():
+            province.population= province.population * 1.01
 
 
+
+
+
+
+
+
+def Simulation_test():
+
+    print(list(m.provincedict.values())[0].population)
+    a= Simulate(m, Queue(population_growth())).simulate(10)
+
+    print(list(m.provincedict.values())[0].population)
+
+
+Simulation_test()
