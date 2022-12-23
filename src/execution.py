00171d3103150e3febba9d0db016a12258e719f6
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-23 00:26:17 -0500

    working on execution

--- src/execution.py
+++ src/execution.py
@@ -1,11 +1,43 @@
-from queue import PriorityQueue
+from elements.map import Map
+from elements.elements import *
 
 
-class Execution:
+#todo: get the execution list
+class Code:
     def __init__(self) -> None:
         self.elements= dict()
-        self.execution_queue = PriorityQueue()
-        
-    def run(self):
-        raise NotImplementedError
+        self.vars= dict()
 
+        self.execution_list = list()
+        self.map= Map()
+
+    def run(self):
+        '''
+        Run the code
+        '''
+        next= self.next()
+        while next:
+            next.execute()
+            next= self.next()
+    
+    def next(self):
+        '''
+        Run the next line of code
+        '''
+        ...
+    
+    def add_element(self, element: str, **kwargs):
+        '''
+        Add an element to the map
+        :param element: the element to add
+        '''
+        elements={
+            'nation': self.map.add_nation,
+            'province': self.map.add_province,
+            'sea': self.map.add_sea,
+            'neutral': self.map.add_neutral
+        }
+        if element in elements:
+            elements[element](**kwargs)
+        else:
+            raise Exception('The element is not recognized')
