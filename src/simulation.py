00eafd64a5708f1c1e564c83663455b868196048
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-23 00:24:58 -0500

    simulation model

--- src/simulation.py
+++ src/simulation.py
@@ -1,12 +1,17 @@
 from events.event import *
 from elements.elements import *
+from elements.elements import *
+from elements.map import Map
 from queue import PriorityQueue
 
 class Queue:
-    def __init__(self):
+    def __init__(self, *args):
         self.queue = PriorityQueue()
         self.events= dict()
         self.event_index= 0
+        
+        for arg in args:
+            self.push((0, arg))
         
 
     def push(self, element: tuple[Time,Event]) -> None:
@@ -71,44 +76,39 @@ class Queue:
 
 #todo: events add events to the queue
 #todo: add decisions to simulation
+#todo: time in Time not int
 class Simulate:
-    def __init__(self):
-        self.event_queue = Queue()
-        self.basic_events()
+    def __init__(self, map: Map, initial_events: Queue):
+        self.event_queue = initial_events
+        self.map = map
 
-    def simulate(self, time: Time):
+    def simulate(self, end_time: int):
         '''
         Run the simulation for a certain amount of time
         :param time: the time the simulation should run
         '''
         while not self.event_queue.empty():
-            if self.event_queue.look()[0] > time:
+            if self.event_queue.look()[0] > end_time:
+                print('break in ', end_time, 'next ', self.event_queue.look()[0])
                 break
 
             for time, event in self.event_queue.pop():
-                event.execute()
+                print(time, event)
+                event.execute(self.map)
                 self.generate_event(event, time)
-                self.decide(event, time)
+                self.decide(map, event, time)
 
-                
 
-    def basic_events(self):
-        '''
-        Add the basic events to the queue
-        Basic events are the events that are always in the simulations
-        '''
-        ...
-
-    def generate_event(self, event: Event, time: Time):
+    def generate_event(self, event: Event, time: int):
         '''
         Generate an event and add it to the queue
         :param event: the event to be generated
         :param time: the time the event should occur
         '''
         if event.is_enabled:
-            self.event_queue.push(time + event.next(), event)
+            self.event_queue.push((time + event.next(), event))
     
-    def decide(self, event: Event, time: Time):
+    def decide(self, map: Map, event: Event, time: int):
         '''
         Decisions of a nation given an event and the moment in which it occurs
         :param event: the event that occurred
