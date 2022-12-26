fbffff0178f94006b1a1a54fee633532f28de10d
lauolivia <91337957+lauolivia@users.noreply.github.com>
2022-12-25 23:28:15 -0500

    working on planning

--- src/ia/planning_decisions.py
+++ src/ia/planning_decisions.py
@@ -7,3 +7,7 @@ def decisions_heuristic(domain, state):
 def decisions_next(domain, state, goal_state, h_values):
     """Get the next states in order of priority"""
     ...
+
+def decisions_goal_state(state):
+    """Check if the state is a goal state."""
+    ...
--- src/ia/search_algorithm.py
+++ src/ia/search_algorithm.py
@@ -18,7 +18,7 @@ class Queue:
 def search(domain, initial_state, goal_state, heuristic):
     return bfsearch(domain, initial_state, goal_state, heuristic)
 
-def bfsearch(domain, initial_state, goal_state, heuristic):
+def bfsearch(domain, initial_state, goal_state, heuristic,case_goal_state):
     """Breadth-first search algorithm."""
     queue = Queue()
     visited = set()
@@ -29,10 +29,10 @@ def bfsearch(domain, initial_state, goal_state, heuristic):
             return state
         if state not in visited:
             visited.add(state)            
-            if(goal_state(state)):
+            if(goal_state(case_goal_state,state)):
                 return state
             h_values= apply_heuristic(domain,state,heuristic) 
-            for next_state in next(domain, state, goal_state, h_values):                
+            for next_state in next(domain, state, h_values):                
                 queue.push()
 
     return state
@@ -42,7 +42,10 @@ def apply_heuristic(domain,state,h):
     """aplicate the heuristic h for the search problem."""
     return h(domain,state)
 
-def next(domain, state, goal_state, h_values):
+def next(domain, state, h_values):
     """Get the next states in order of priority"""
     ...
 
+def goal_state(case_goal_state,state):
+    """Check if the state is a goal state."""
+    return case_goal_state(state)
