e8d77ca8f6d9b869b2d2f478d472a557bc5157fe
lauolivia <91337957+lauolivia@users.noreply.github.com>
2022-12-25 23:25:11 -0500

    working on planning

---
+++ src/ia/planning_decisions.py
@@ -0,0 +1,9 @@
+
+
+def decisions_heuristic(domain, state):
+    """Heuristic for the decisions problem."""
+    ...
+
+def decisions_next(domain, state, goal_state, h_values):
+    """Get the next states in order of priority"""
+    ...
---
+++ src/ia/search_algorithm.py
@@ -0,0 +1,48 @@
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        
+    def push(self, element):        
+        self.queue.append(element)
+    
+    def peek(self):
+        return self.queue[0]
+    
+    def pop(self):
+        return self.queue.pop(0)
+    
+    def empty(self):
+        return len(self.queue) == 0
+
+def search(domain, initial_state, goal_state, heuristic):
+    return bfsearch(domain, initial_state, goal_state, heuristic)
+
+def bfsearch(domain, initial_state, goal_state, heuristic):
+    """Breadth-first search algorithm."""
+    queue = Queue()
+    visited = set()
+    queue.push((0, initial_state))
+    while not queue.empty():
+        state = queue.pop()
+        if goal_state(state):
+            return state
+        if state not in visited:
+            visited.add(state)            
+            if(goal_state(state)):
+                return state
+            h_values= apply_heuristic(domain,state,heuristic) 
+            for next_state in next(domain, state, goal_state, h_values):                
+                queue.push()
+
+    return state
+
+
+def apply_heuristic(domain,state,h):
+    """aplicate the heuristic h for the search problem."""
+    return h(domain,state)
+
+def next(domain, state, goal_state, h_values):
+    """Get the next states in order of priority"""
+    ...
+
