36cce18f54867999c60ff9c858708776770fd818
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-28 14:27:05 -0500

    working on compiler

--- src/compiler/compiler.py
+++ src/compiler/compiler.py
@@ -4,13 +4,18 @@ class NDSLexer(Lexer):
     tokens = {'NATION', 'PROVINCE', 'SEA', 'NEUTRAL', 'EVENT', 'DISTRIBUTION',
             'NAME',
             'NUMBER',
-            'ASSIGN', 'LINEEND', 'COMMA',
-            'LPAREN', 'RPAREN', 'LBRAKET', 'RBRAKET', 'LKEY', 'RKEY',
+            'ASSIGN',
             'FOR', 'WHILE', 'IF', 'ELSE',
             'GREATER', 'LESS', 'XPLUS', 'XMINUS',
             'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS'}
-    ignore = "\t \n"
+    
+    literals = { '(', ')', '{', '}', '[', ']', ';', ',' }
 
+    ignore = "\t "
+
+    newline = r'\n+'
+    def newline(self, t):
+        self.lineno += t.value.count('\n')
 
     #ELEMENTS
     NATION= r'nation\s+'
@@ -76,15 +81,7 @@ class NDSLexer(Lexer):
 
     # Special symbols
     ASSIGN = r':'
-    LINEEND= r';'
-    COMMA= r','
 
-    LPAREN = r'\('
-    RPAREN = r'\)'
-    LBRAKET= r'\['
-    RBRAKET= r'\]'
-    LKEY= r'\{'
-    RKEY= r'\}'
 
     XPLUS= r'\+\+'
     XMINUS= r'--'
@@ -99,12 +96,7 @@ class NDSLexer(Lexer):
     LESS= r'<'
 
     # Ignored pattern
-    ignore_newline = r'\n+'
     ignore_comment = r'\#.*'
-
-    # Extra action for newlines
-    def ignore_newline(self, t):
-        self.lineno += t.value.count('\n')
 
     def error(self, t):
         print("Illegal character '%s'" % t.value[0])
@@ -129,12 +121,23 @@ class NDSParser(Parser):
             'provinces': {},
             'seas': {},
             'neutrals': {},
-        } 
+        }
+    
+    @_('script ";" script')
+    def script(self, p):
+        return p.script0 + p.script1
+    
+    @_('script ";"')
+    def script(self, p):
+        return p.script
+    
+    @_('element')
+    def script(self, p):
+        return p.element
 
-
-    @_('NATION NAME params LINEEND', 'PROVINCE NAME params LINEEND', 'SEA NAME params LINEEND', 'NEUTRAL NAME params LINEEND', 'element')
+    @_('NATION NAME params', 'PROVINCE NAME params', 'SEA NAME params', 'NEUTRAL NAME params')
     def element(self, p):
-        print(1, p.NAME, p.params)
+        # print(1, p.NAME, p.params)
         if p[0] == 'nation':
             self.elements['nations'][p.NAME] = p.params
             return p.NAME
@@ -149,44 +152,37 @@ class NDSParser(Parser):
             return p.NAME
     
 
-    @_('LPAREN param RPAREN')
+    @_('"(" param ")"')
     def params(self, p):
-        print(2, p.param)
+        # print(2, p.param)
         return p.param
 
-    @_('param COMMA param')
+    @_('param "," param')
     def param(self, p):
-        print(3, p.param0, p.param1)
+        # print(3, p.param0, p.param1)
         return p.param0 + p.param1
-    
+        
     @_('NUMBER')
     def param(self, p):
-        print(4, p.NUMBER)
+        # print(4, p.NUMBER)
         return [int(p.NUMBER)]
 
 
 
 
 
-def main(code: str):
+def compile(code: str):
     lexer = NDSLexer()
-    tokens = lexer.tokenize(code)
-
     parser = NDSParser()
-    exe_line= []
-    for i in tokens:
-        if i.type != 'LINEEND':
-            exe_line.append(i)
-        else:
-            exe_line.append(i)
-            parser.parse(iter(exe_line))
-            exe_line= []
+
+    tokens = lexer.tokenize(code)
+    parser.parse(tokens)
     print(parser.elements)
 
-
-main(
+compile(
     '''
-    nation a(2, 3, 5);
+    nation a(2, 3,
+    5);
     nation b(3);
     province c(4);
     nation d(5);
