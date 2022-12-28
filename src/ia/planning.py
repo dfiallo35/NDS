d380ca53dfd5f12e13ef6454ad54d544ac2b43aa
lauolivia <91337957+lauolivia@users.noreply.github.com>
2022-12-28 01:02:00 -0500

    working on planning

--- src/ia/planning.py
+++ src/ia/planning.py
@@ -1,8 +1,8 @@
 
 
 class Problem:
-    def __init__(self) -> None:
-        self.actions:dict()#dict that contains actions with their expected result
+    def __init__(self,actions) -> None:
+        self.actions=actions
 
 
     def goal_state(self,state):
@@ -14,3 +14,38 @@ class Problem:
     def apply_action_to_state(self,action,state):
         """return the new state after apply it an action"""
         ...
+
+
+class PlanningProblem(Problem): 
+
+    def __init__(self, initial, actions):
+        super().__init__(actions)
+        self.initial = initial
+        self.actions = actions
+
+class Action:
+    
+    def __init__(self, action, precond, effect ):
+       self.action=action
+       self.precond=precond
+       self.effect=effect
+
+    def check_preconds(self,action):
+        ...
+    
+    def make_action(self):
+        ...
+
+class StateNode:
+    def __init__(self,state,father):
+        self.state=state
+        self.father=father
+        self.sons=[]
+    
+    def get_father(self):
+        return self.father
+    
+    
+        
+
+      
