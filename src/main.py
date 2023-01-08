703dcc1a1687341457af7df07a952ba6841dce37
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2023-01-08 16:05:56 -0500

    fixed key bug

--- src/main.py
+++ src/main.py
@@ -20,12 +20,10 @@ def map(map: Map):
 
 
 class CodeBlock:
-    def __init__(self, code: Code, number: int, script: str= ''):
+    def __init__(self, key: str, code: Code):
         self.code= code
-        self.number= number
         self.intern_code= copy(self.code)
-        self.script= script
-        
+        self.key= str(key)
         
 
     def run(self):
@@ -33,14 +31,14 @@ class CodeBlock:
         
         with self.col1:
             em= st.empty()
-            self.script= em.text_area(f'code', key=f'code {self.number}', placeholder='code here', height=200, value=self.script, label_visibility='hidden')
+            self.script= em.text_area(f'code', key=self.key + '1', placeholder='code here', height=200)
 
         
         with self.col2:
             st.markdown('##')
-            st.button('Run', key=f'Run {self.number}', on_click=self.execute)
-            st.button('Delete', key=f'Delete {self.number}', on_click=self.delete_code_block)
-            st.button('Add', key=f'Add {self.number +1}', on_click=self.add_code_block)
+            st.button('Run', key=self.key + '2', on_click=self.execute)
+            st.button('Delete', key=self.key + '3', on_click=self.delete_code_block)
+            st.button('Add', key=self.key + '4', on_click=self.add_code_block)
 
         st.markdown('##')
     
@@ -67,67 +65,37 @@ class CodeBlock:
         for code_block in st.session_state.code_blocks:
             if code_block.number > self.number:
                 code_block.code= copy(self.intern_code)
-                
-        
+    
 
     def add_code_block(self):
         cb= []
         for code_block in st.session_state.code_blocks:
-            if code_block.number == self.number:
-                cb.append(CodeBlock(copy(code_block.code), code_block.number, code_block.script))
-
-                rest_code_blocks= [(copy(i.code), i.number, i.script)for i in st.session_state.code_blocks[self.number+1:]]
-                st.session_state.code_blocks.clear()
-                if len(rest_code_blocks) == 0:
-                    cb.append(CodeBlock(copy(self.intern_code), self.number +1))
-                    break
-
-                for above_code_block in rest_code_blocks:
-                    if above_code_block[1] == self.number +1:
-                        
-                        temp_cb= CodeBlock(copy(self.intern_code), above_code_block[1], '')
-                        cb.append(temp_cb)
-
-                        temp_cb= CodeBlock(copy(self.intern_code), above_code_block[1]+1, above_code_block[2])
-                        cb.append(temp_cb)
-
-                    else:
-                        temp_cb= CodeBlock(copy(self.intern_code), above_code_block[1]+1, above_code_block[2])
-                        cb.append(temp_cb)
-                
-                break
+            if code_block == self:
+                cb.append(code_block)
+                cb.append(CodeBlock(key=time.time(), code=copy(self.intern_code)))
             else:
-                cb.append(CodeBlock(copy(code_block.code), code_block.number, code_block.script))
+                cb.append(code_block)
 
         st.session_state.code_blocks= cb
     
-    #todo: if just one code block, dont delete
+    
+
     def delete_code_block(self):
-        cb= []
         if len(st.session_state.code_blocks) == 1:
             return
+        
+        cb= []
         for code_block in st.session_state.code_blocks:
-            if code_block.number == self.number:
-                rest_code_blocks= [(copy(i.code), i.number, i.script)for i in st.session_state.code_blocks[self.number+1:]]
-                st.session_state.code_blocks.clear()
-                if len(rest_code_blocks) == 0:
-                    break
-
-                for above_code_block in rest_code_blocks:
-                    temp_cb= CodeBlock(copy(self.intern_code), above_code_block[1]-1, above_code_block[2])
-                    cb.append(temp_cb)
-                
-                break
+            if code_block == self:
+                continue
             else:
-                cb.append(CodeBlock(copy(code_block.code), code_block.number, code_block.script))
-
+                cb.append(code_block)
         st.session_state.code_blocks= cb
-
 
 class Visualizer:
     def visualize(self):
         if 'code_blocks' not in st.session_state:
-            st.session_state.code_blocks= [CodeBlock(Code(), 0)]
+            st.session_state.code_blocks= [CodeBlock(key=time.time(), code=Code())]
         st.title('Nations Development Simulator')
 
         st.sidebar.title('NDS')
